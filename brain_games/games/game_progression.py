bc199d881a7d20f1243eab160f4793f70590a546
DenisTimer <denis1timerkaev@gmail.com>
2026-07-07 21:58:26 +0300

    fix brain_games/games/game_progression.py

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -3,21 +3,21 @@ from brain_games.generation import get_number
 
 def get_progression(start, step, length):
     progression = []
-    hidden_index = get_number(0, length - 1)
     correct_answer = 0
+    index_answer = get_number(0, length - 1)
     for index in range(length):
-        current_element = start + index * step
-        if index == hidden_index:
+        ellement = start + index * step
+        if index == index_answer:
             progression.append('..')
-            correct_answer = current_element
+            correct_answer = ellement
         else:
-            progression.append(str(current_element))
-    return progression, correct_answer
+            progression.append(str(ellement))
+    return progression, correct_answer 
 
 
 def question_and_answer_progression():
     start = get_number()
     step = get_number(1, 10)
     length = get_number(5, 12)
-    question_text, correct_answer = get_progression(start, step, length)
-    return ' '.join(question_text), str(correct_answer)
+    qwestion_text, correct_answer = get_progression(start, step, length)
+    return ' '.join(qwestion_text), str(correct_answer)
